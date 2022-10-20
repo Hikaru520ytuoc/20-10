1fec7603bd1cd9e106e9ff14dec151cc374b251e
Hikaru520ytuoc <114506954+Hikaru520ytuoc@users.noreply.github.com>
2022-10-20 21:33:51 +0700

    Add files via upload
    ff

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bn của tui';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bn của tui';
+const contentCard = ' Tui chúc cô bn của tui luôn luôn xinh đẹp,tươi cười, thành công trên đoạn đường phía trước và đừng bỏ quên tui nhé. Chúc cô bn đáng yêu của tui ngày 20/10 thật vui vẻ, hạnh phúc!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 't.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
